f2e269b1ab311523a369e8bc83fdac19b2f88f44
mohammed imran <myselfmdimran@gmail.com>
2020-12-24 23:28:08 +0530

    Modified API

--- src/SubjectData.jsx
+++ src/SubjectData.jsx
@@ -8,14 +8,14 @@ const SubjectData = [
         imgsrc:dbms,
         title:"DataBase Management System",
         code:"18CS53",
-        link:"https://damassvit.blogspot.com/",
+        link:"https://takeiteasyengineers.com/category/cse-ise/5th-sem/dbms/",
     },
 
     {
         imgsrc:dbms,
-        title:"DataBase Management System",
-        code:"18CS53",
-        link:"https://damassvit.blogspot.com/",
+        title:"UNIX Programming",
+        code:"18CS5",
+        link:"https://takeiteasyengineers.com/category/cse-ise/5th-sem/up/",
     },
 
     {
